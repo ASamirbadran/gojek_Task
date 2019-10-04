bfc2b46d51d5974afa783eac797460f936dd84b5
ahmed.samir <ahmed.samir@ibtikar.net.sa>
2019-10-04 13:00:02 +0200

    Project Structure Type: STORY #Number
    Work Done :
    ===========
    - Adding Persistence Layer For UserDefaults

---
+++ Contacts/Services/Persistence Manager/KeyValuePersistence.swift
@@ -0,0 +1,47 @@
+//
+//  KeyValuePersistence.swift
+//  Contacts
+//
+//  Created by Ahmed Samir on 10/4/19.
+//  Copyright © 2019 GoJek. All rights reserved.
+//
+
+import Foundation
+
+public class PersistenceKeyBase {
+}
+
+#if DEBUG
+private struct Statics {
+    static var registeredKeys = Set<String>()
+}
+#endif
+
+public final class PersistenceKey<Type>: PersistenceKeyBase {
+    public let key: String
+    public let defaultValue: Type
+
+    public init(key: String, defaultValue: Type) {
+        self.key = key
+        self.defaultValue = defaultValue
+
+        #if DEBUG
+        if Statics.registeredKeys.contains(key) {
+            fatalError("PersistenceKey '\(key)' is registered multiple times")
+        }
+        Statics.registeredKeys.insert(key)
+        #endif
+    }
+
+    fileprivate init(_ key: String, _ defaultValue: Type) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+}
+
+public protocol KeyValuePersistence {
+
+    static func valueForKey<T>(_ key: PersistenceKey<T>) -> T
+    static func setValue<T>(_ value: T?, forKey key: PersistenceKey<T>)
+    static func removeValueForKey<T>(_ key: PersistenceKey<T?>)
+}
